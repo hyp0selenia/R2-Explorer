6f0756f79b08a54d3ce2c92fe16210919517e3ee
hyp0selenia <how2pl4y@outlook.com>
2026-09-05 06:19:22 +0800

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { R2Explorer } from 'r2-explorer';
+
+export default R2Explorer({ readonly: false, basicAuth: [{
+    username: 'admin',
+    password: 'password'
+  },{
+    username: 'wuwenzhen',
+    password: 'wuwenzhen'
+  }]
+});
